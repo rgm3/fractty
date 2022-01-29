4ade4c199483ca11808e8c0c9b6bfcb9d19b8269
rgm <211502+rgm3@users.noreply.github.com>
2022-01-29 16:37:51 -0600

    Update to tcell/v2, improve mouse & KB usability

--- go.mod
+++ go.mod
@@ -1,8 +1,8 @@
 module github.com/rgm3/gomandelbrot
 
-go 1.13
+go 1.16
 
 require (
-	github.com/gdamore/tcell v1.3.0
-	gopkg.in/DATA-DOG/go-sqlmock.v1 v1.3.0 // indirect
+	github.com/gdamore/tcell v1.4.0
+	github.com/gdamore/tcell/v2 v2.4.0
 )
--- main.go
+++ main.go
@@ -6,8 +6,8 @@ import (
 	"os"
 	"time"
 
-	"github.com/gdamore/tcell"
 	"github.com/gdamore/tcell/encoding"
+	"github.com/gdamore/tcell/v2"
 )
 
 var style = tcell.StyleDefault
@@ -41,7 +41,7 @@ func main() {
 	go func() {
 		for {
 			drawScreen(s)
-			time.Sleep(time.Millisecond * 10)
+			time.Sleep(time.Millisecond * 100)
 		}
 	}()
 
@@ -68,8 +68,8 @@ var vp = NewViewport(-2.0, 1.0, -1.0, 1.0)
 func zoom(s tcell.Screen, direction, x, y int) {
 	//	w, h := s.Size()
 
-	factorx := (vp.x0 - vp.x1) / 10.0
-	factory := (vp.y0 - vp.y1) / 10.0
+	factorx := (vp.x0 - vp.x1) / 20.0
+	factory := (vp.y0 - vp.y1) / 20.0
 
 	if direction == 1 {
 		vp.x0 -= factorx
@@ -97,32 +97,37 @@ func drawScreen(s tcell.Screen) {
 
 	for x := 0; x < w; x++ {
 		for y := 0; y < h*2; y += 1 {
+
+			// convert terminal window character cell (double height) to viewport range
 			r := mapnum(x, 0, w, vp.x0, vp.x1)
 			i := mapnum(y, 0, h*2, vp.y0, vp.y1)
 			i2 := mapnum(y+1, 0, h*2, vp.y0, vp.y1)
 
-			converges, iter := calc_spot(r, i)
+			// top half of character cell uses background color
+			converges, iter := isConvergent(r, i)
 			if converges {
-				st = st.Background(tcell.Color(232))
+				st = st.Background(tcell.ColorBlack)
 			} else {
 				st = st.Background(asColor(iter))
 			}
 
-			converges, iter = calc_spot(r, i2)
+			// bottom half of character cell uses foreground color
+			converges, iter = isConvergent(r, i2)
 			if converges {
-				st = st.Foreground(tcell.Color(232))
+				st = st.Foreground(tcell.ColorBlack)
 			} else {
 				st = st.Foreground(asColor(iter))
 			}
 
-			s.SetCell(x, y/2, st, gl)
+			s.SetContent(x, y/2, gl, nil, st)
 		}
 	}
 
 	s.Show()
 }
 
-func calc_spot(ca, cb float64) (bool, int) {
+// Whether or not the given point is in the Mandelbrot set
+func isConvergent(ca, cb float64) (bool, int) {
 	var a, b float64 = 0, 0
 	max := 1000
 	var i int
@@ -137,12 +142,33 @@ func calc_spot(ca, cb float64) (bool, int) {
 }
 
 func asColor(n int) tcell.Color {
-	color := tcell.Color((16 + n) % 229)
+	color := tcell.PaletteColor((16 + n) % 229)
 	return color
 }
 
 func mapnum(x, in_min, in_max int, out_min, out_max float64) float64 {
 	return (float64(x)-float64(in_min))*(out_max-out_min)/(float64(in_max)-float64(in_min)) + out_min
+}
+
+func moveToCell(x, y int, s tcell.Screen) {
+	w, h := s.Size()
+
+	// 0,0 is the upper-left corner
+
+	// Translate terminal cell location to viewport coordinates
+
+	vpdx := vp.x1 - vp.x0
+	vpdy := vp.y1 - vp.y0
+
+	vp.x0 += vpdx * float64(x) / float64(w)
+	vp.x0 -= vpdx / 2
+	vp.x1 = vp.x0 + vpdx
+
+	vp.y0 += vpdy * float64(y) / float64(h)
+	vp.y0 -= vpdy / 2
+	vp.y1 = vp.y0 + vpdy
+
+	return
 }
 
 func pollEvents(s tcell.Screen) {
@@ -151,18 +177,38 @@ func pollEvents(s tcell.Screen) {
 		switch ev := ev.(type) {
 		case *tcell.EventKey:
 			switch ev.Key() {
-			case tcell.KeyEscape, tcell.KeyEnter:
+			case tcell.KeyEscape:
 				close(quit)
 				return
 			case tcell.KeyRune:
 				switch ev.Rune() {
-				case 'z':
+				case 'z', '+', '=':
 					zoom(s, 1, 1, 1)
-				case 'x':
+				case 'x', '-', '_':
 					zoom(s, 0, 1, 1)
 				case 'q':
 					close(quit)
 					return
+
+					// TODO: DRY
+				case 'w':
+					step := (vp.y0 - vp.y1) / 10
+					vp.y0 += step
+					vp.y1 += step
+
+				case 'a':
+					step := (vp.x0 - vp.x1) / 10
+					vp.x0 += step
+					vp.x1 += step
+				case 's':
+					step := (vp.y0 - vp.y1) / 10
+					vp.y0 -= step
+					vp.y1 -= step
+				case 'd':
+					step := (vp.x0 - vp.x1) / 10
+					vp.x0 -= step
+					vp.x1 -= step
+
 				}
 				//s.Sync()
 			case tcell.KeyUp:
@@ -181,19 +227,37 @@ func pollEvents(s tcell.Screen) {
 				step := (vp.x0 - vp.x1) / 10
 				vp.x0 -= step
 				vp.x1 -= step
+			case tcell.KeyHome:
+				// vp.reset()
+				vp.x0 = -2.0
+				vp.x1 = 1.0
+				vp.y0 = -1.0
+				vp.y1 = 1.0
+			case tcell.KeyPgUp:
+				zoom(s, 1, 1, 1)
+			case tcell.KeyPgDn:
+				zoom(s, 0, 1, 1)
+
 			}
 		case *tcell.EventMouse:
 			x, y := ev.Position()
-			button := ev.Buttons()
+			//button := ev.Buttons()
 			/*if button&tcell.WheelUp != 0 {
 				bstr += " WheelUp"
 			}*/
 			// Only buttons, not wheel events
-			button &= tcell.ButtonMask(0xff)
+			//button &= tcell.ButtonMask(0xff)
 			switch ev.Buttons() {
-			case tcell.Button1:
+			case tcell.ButtonPrimary:
+				moveToCell(x, y, s)
 				zoom(s, 1, x, y)
-			case tcell.Button2:
+			case tcell.WheelUp:
+				//moveToCell(x/10, y/10, s)
+				zoom(s, 0, 1, 1)
+			case tcell.WheelDown:
+				//moveToCell(x/10, y/10, s)
+				zoom(s, 1, 1, 1)
+			case tcell.ButtonSecondary:
 				zoom(s, 0, x, y)
 			}
 		case *tcell.EventResize:
